15ab29778d1b1171a3968f245212bd6b13429faa
c32tao@uwaterloo.ca <c32tao@uwaterloo.ca>
2023-09-17 03:39:37 -0400

    everything

--- routes/background.js
+++ routes/background.js
@@ -1,54 +1,56 @@
-// const router = require("express").Router();
-// const bcrypt = require("bcrypt");
-// const User = require("../models/user");
-// const jwt = require("jsonwebtoken");
-// const axios = require("axios");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
 
-// //prompt
-// router.post("/:email", async (req, res) => {
-//   try {
-//     const user = await User.findOne({ email: req.params.email });
-//     if (user) {
-//       try {
-//         await User.updateOne({
-//           backgroundImage: req.body.backgroundUrl,
-//         });
+//prompt
+router.post("/:email", async (req, res) => {
+  try {
+    console.log("backgrounding", req.body.backgroundUrl);
+    const user = await User.findOne({ email: req.params.email });
+    console.log(user);
+    if (user) {
+      console.log("i'm here", user);
 
-//         console.log("user", user);
-//         res.status(200).json({
-//           backgroundDoneUpdate: true,
-//         });
-//       } catch (error) {
-//         res.status(500).json({
-//           backgroundDoneUpdate: false,
-//         });
-//         console.error(error);
-//       }
-//     }
-//   } catch (err) {
-//     res.status(500).json({
-//       error: err,
-//     });
-//   }
-// });
+      try {
+        await User.updateOne(
+          { email: req.params.email },
+          { backgroundImage: req.body.backgroundUrl }
+        );
 
-// // router.get("/:email", async (req, res) => {
-// //   console.log("byebye1");
-// //   try {
-// //     const user = await User.findOne({ email: req.params.email });
-// //     console.log("user.products", user.products);
+        res.status(200).json({
+          backgroundDoneUpdate: true,
+        });
+      } catch (error) {
+        res.status(500).json({
+          backgroundDoneUpdate: false,
+        });
+        console.error(error);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
+  }
+});
 
-// //     if (user) {
-// //       res.json({
-// //         productUrls: user.products,
-// //       });
-// //     }
-// //   } catch (err) {
-// //     res.status(500).json({
-// //       error: err,
-// //       message: "Invalid token",
-// //     });
-// //   }
-// // });
+router.get("/:email", async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.params.email });
+  
+      if (user) {
+        res.json({
+          backgroundUrl: user.backgroundImage,
+        });
+      }
+    } catch (err) {
+      res.status(500).json({
+        error: err,
+        message: "Invalid token",
+      });
+    }
+  });
 
-// module.exports = router;
+module.exports = router;
